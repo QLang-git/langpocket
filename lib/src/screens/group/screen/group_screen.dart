98d7bfc3eaffdbbad978df88e1bc93d791d61ebb
aliamerj <aliamer19ali@gmail.com>
2023-02-25 21:06:12 +0300

    fix routing bugs

--- lib/src/screens/group/screen/group_screen.dart
+++ lib/src/screens/group/screen/group_screen.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:langpocket/src/common_widgets/responsive_center.dart';
+import 'package:langpocket/src/data/local/repository/drift_group_repository.dart';
 import 'package:langpocket/src/screens/group/app_bar/group_appbar.dart';
 import 'package:langpocket/src/screens/group/widgets/words_list.dart';
 import 'package:langpocket/src/utils/constants/breakpoints.dart';
 
 class GroupScreen extends StatefulWidget {
-  final String groupId;
-  final String groupName;
-  final String date;
-  const GroupScreen(
-      {super.key,
-      required this.groupId,
-      required this.groupName,
-      required this.date});
+  final GroupData groupData;
+  const GroupScreen({
+    super.key,
+    required this.groupData,
+  });
 
   @override
   State<GroupScreen> createState() => _GroupScreenState();
@@ -22,17 +20,19 @@ class GroupScreen extends StatefulWidget {
 class _GroupScreenState extends State<GroupScreen> {
   @override
   Widget build(BuildContext context) {
+    final groupDate = widget.groupData.creatingTime;
+    final dateFormat = '${groupDate.day}/${groupDate.month}/${groupDate.year}';
     return ResponsiveCenter(
       child: Scaffold(
         appBar: GroupAppBar(
-          groupName: widget.groupName,
-          groupDate: widget.date,
-          groupId: int.parse(widget.groupId),
+          groupName: widget.groupData.groupName,
+          groupDate: dateFormat,
+          groupId: widget.groupData.id,
         ),
         body: WordsGroups(
-          groupId: int.parse(widget.groupId),
-          groupName: widget.groupName,
-          date: widget.date,
+          groupId: widget.groupData.id,
+          groupName: widget.groupData.groupName,
+          date: dateFormat,
         ),
         floatingActionButton: SpeedDial(
           animatedIcon: AnimatedIcons.menu_close,
